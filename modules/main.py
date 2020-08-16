c6b0bd4e5178ca9683448a9332b1b280d4da51cb
Taguar258 <36562445+Taguar258@users.noreply.github.com>
2020-08-16 19:40:03 +0200

    Update main.py
    Minor change to banner

--- modules/main.py
+++ modules/main.py
@@ -50,8 +50,8 @@ C_G (
 |   // _` |\\ V / / -_)| ' \\)) (_-<|  _|/ _ \\| '_|| '  \\C_G()C_V
 |_|_\\\\__,_| \\_/  \\___||_||_|  /__/ \\__|\\___/|_|  |_|_|_|C_W
  
-C_BoldStress-Testing-Toolkit by Taguar258 | Copyright (c) Taguar258 MIT 2020
-Based on the CLIF Framework by Taguar258 | Copyright (c) Taguar258 MIT 2020C_White
+C_BoldStress-Testing-Toolkit by Taguar258 (c) | MIT 2020
+Based on the CLIF Framework by Taguar258 (c) | MIT 2020C_W
 
 The creators of Raven-Storm are not responsible
 for any of your activitys or issues caused by Raven-Storm!
