14787ef15a3ea2a2de8eedc6af15e06be5a312dd
Taguar258 <taguar466@gmail.com>
2020-11-07 23:46:27 +0100

    Not important change

--- Raven-Storm/uninstall.sh
+++ Raven-Storm/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "[i] We will now uninstall Raven-Storm..."
 echo "[i] This will delete all backups."
-sudo rm -i -rf /usr/share/Raven-Storm
 sudo rm -i /usr/bin/rst
+sudo rm -rf -i /usr/share/Raven-Storm
 
 echo "[i] Raven-Storm sucessfully uninstalled."
 exit 0
